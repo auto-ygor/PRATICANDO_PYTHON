58c2699b2b33c4f9b96562faa6d80fdae6002cfc
Ygor Queiroz <ygorqueiroz619@gmail.com>
2025-01-24 00:01:36 -0300

    Atualizado jogo_da_senha.py
    O que mudou?
    
    Função menu(): Agora, ao invés de informar o número 1 para iniciar o jogo, só é necessário pressionar a tecla "Enter".
    
    Função opcao(): Esta função foi removida em decorrência da alteração na função menu(). Não é mais preciso obter a opção.
    
    Função limpar_tela(): Anteriormente, só existia o comando system("cls) para limpar a tela, mas esse comando só funciona para o sistema operacional Windows. Agora, esta função verifica o sistema operacional do usuário e executa a função correspondente ao mesmo para limpar a tela do terminal.
    
    Função jogo_da_senha(): Foi implementada tentativas para que o jogo fique mais difícil. São 5 tentativas, caso o usuário erre o dígito e as tentativas fiquem zeradas, ele perde o jogo.

--- PRATICANDO_/jogo_da_senha.py
+++ PRATICANDO_/jogo_da_senha.py
@@ -1,5 +1,5 @@
 from random import randint
-from os import system
+from os import system, name
 from time import sleep
 
 
@@ -9,39 +9,25 @@ def pausar():
 
 def menu():
     print("----------------- ADIVINHE A SENHA -------------------")
-    print("Obs: O tamanho da senha deve ser entre 4 e 8 dígitos.")
+    print("Obs: O tamanho da senha deve ser de 4 a 8 dígitos.")
     print("------------------------------------------------------")
     print("Forma de jogar: Tente adivinhar o primeiro dígito.\n"
           "Se acertar, tente o segundo e assim por diante.")
     print("------------------------------------------------------")
-    print("[1] Iniciar")
-    print("------------------------------------------------------")
+    input("Pressione ENTER para continuar...")
+    limpar_tela()
 
 
 def limpar_tela():
-    system("cls")
+    '''
+    Executa a função de limpar a tela do terminal
+    de acordo com o sistema operacional do usuário.
+    '''
+    if name == 'nt': #Windows
+        system('cls')
+    else: #Linux, MacOS, etc
+        system('clear')
     
-
-def opcao():
-    while True:
-        try:
-            menu()
-            opcao = int(input("Digite a opção desejada: "))      
-            if opcao == 1:
-                limpar_tela()
-                break
-            else:
-                limpar_tela()
-                print("Digite uma opção válida.")
-                pausar()
-                limpar_tela()
-        except	ValueError:
-            limpar_tela()
-            print("Digite algo válido.")
-            pausar()
-            limpar_tela()
-            continue
-
 
 def tamanho_senha(tam):
     while True:
@@ -71,7 +57,7 @@ def tamanho_senha(tam):
     
               
 def jogo_da_senha():
-    opcao()
+    menu()
     tamanho = tamanho_senha(0)
     
     #Cria duas listas vazias:
@@ -84,10 +70,22 @@ def jogo_da_senha():
         senha_criptografada.append('*')
        
     contador = 0
+    quant_tentativas = 5
     
     while contador != tamanho:
         print(f"A senha é: {' | '.join(senha_criptografada)}")
         print("-----------------------------------------------------------")
+        print(f"Você tem {quant_tentativas} tentativas.")
+        print("-----------------------------------------------------------")
+        
+        '''
+        Se o número de tentativas for igual a 0, o jogo é finalizado e a senha é exibida.
+        '''
+        if quant_tentativas == 0:
+            limpar_tela()
+            print("Você perdeu todas as tentativas.")
+            print(f"A senha era: {' | '.join(map(str, senha))}")
+            break
         
         try:
             tentativa_user = int(input(f"Adivinhe o {contador + 1}º dígito da senha: "))
@@ -100,20 +98,23 @@ def jogo_da_senha():
             else:
                 '''
                 Se o número digitado for igual ao número da senha, o '*' é substituído pelo número
-                e o contador é incrementado:
+                e o contador é incrementado. A variável "quant_tentativas" é reiniciada para 5.
                 '''                
                 if tentativa_user == senha[contador]:
                     senha_criptografada[contador] = str(tentativa_user)
                     contador+= 1
+                    quant_tentativas = 5
                     print("Você acertou o dígito!")
                     pausar()
                     limpar_tela()
                 #Se o número digitado for diferente do número da senha:
-                elif tentativa_user > senha[contador]:        
+                elif tentativa_user > senha[contador]:
+                    quant_tentativas -= 1        
                     print("O número é menor.")
                     pausar()
                     limpar_tela()
                 else:
+                    quant_tentativas -= 1
                     print("O número é maior.")
                     pausar()
                     limpar_tela()
@@ -127,12 +128,12 @@ def jogo_da_senha():
     
     '''
     Quando o contador se igualar ao tamanho da senha, o jogo é finalizado e a mensagem
-    de conclusão é exibida:
-    '''       
-    limpar_tela()
-    print("Parabéns, Você acertou a senha!!")
-    print(f"A senha era: {' | '.join(map(str, senha))}")
-
+    de conclusão é exibida. Esta mensagem só será exibida se o usuário acertar a senha.
+    '''
+    if contador == tamanho:       
+        limpar_tela()
+        print("Parabéns, Você acertou a senha!!")
+        print(f"A senha era: {' | '.join(map(str, senha))}")
 
 #Inicializa o jogo:      
 jogo_da_senha()
