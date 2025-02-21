35e48b1ad77fb041d1938e217dbd3506077030ef
Ygor Queiroz <ygorqueiroz.oficial@gmail.com>
2025-02-21 12:31:25 -0300

    Pequenas alterações

--- PRATICANDO_/help.py
+++ PRATICANDO_/help.py
@@ -1,12 +1,8 @@
 #Neste arquivo, é possível entender o que todas as funções contidas na pasta "praticando_" fazem!!
 
 '''
-Para importar as funções de determinado arquivo/exercício, cheque a pasta 
+Para checar as funções de determinado arquivo/exercício, abra a pasta 
 em que ele está e veja um arquivo python chamado "func_nomeDoArquivo.py".
-
-Exemplo de como importar as funçoes do arquivo escolhido: from func_nomeDoArquivo/Exercicio import *
-
-Caso queira importar funções específicas: from func_nomeDoArquivo/Exercicio import funcao_escolhida
 '''
 
 #Exemplo (mude conforme os exemplos acima):
